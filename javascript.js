2960ad97e1ed7558be40d5920212e2a1478480ca
Németh Csaba Bence <71324001+Benceplays@users.noreply.github.com>
2022-04-09 20:55:34 +0200

    par cucc

--- javascript.js
+++ javascript.js
@@ -1,11 +1,28 @@
+let on = true;
 function bejelentkezes(){
     var username = document.getElementById("uname").value;
     var password = document.getElementById("psw").value;
     if (username == "Booby" && password == "Booby" || username == "DaW33D" && password == "DaW33D" || username == "ZoroXHun" && password == "ZoroXHun"){
-        alert("Sikeres bejelentkezés!");
         window.location = ("adminpanel.html");
     }
     else{
         alert("Sikertelen bejelentkezés!");
     }
+}
+function csuszi(element, x, y) {
+    ontitle = "ON"
+    offtitle = "OFF"
+    if (on == true){
+        document.getElementById("csuszi").style.position="relative";
+        document.getElementById("csuszi").style.left="45%";
+        document.getElementById("ontitle").innerHTML = offtitle;
+        on = false;
+    }
+    else{
+        document.getElementById("csuszi").style.position="relative";
+        document.getElementById("csuszi").style.left="0%";
+        document.getElementById("ontitle").innerHTML = ontitle;
+        on = true;
+    }
+    console.log(on)
 }
